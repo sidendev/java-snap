33da1fcc3f0ac4a10673c236a8f33ef0621959f3
sidendev <96108993+sidendev@users.noreply.github.com>
2025-02-13 17:40:40 +0000

    Set up playGame method in Snap class, while loop to run through dealCard, checking and adding to Arraylist

--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -1,20 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Snap extends CardGame {
-    private Snap(String snap) {
-        super(snap);
+    private ArrayList<Card> dealtCards;
+    private Scanner scanner;
+
+    // Constructor
+    public Snap() {
+        super("Snap");
+        this.dealtCards = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
     }
+
+    // playGame method
+    public void playGame() {
+        System.out.println("Welcome to Java Snap!");
+        System.out.println("How to play Java Snap:");
+        System.out.println("Press Enter to be dealt a card.");
+        System.out.println("If two consecutive cards match, you win!");
+        System.out.println("Press Enter to begin...");
+
+        scanner.nextLine(); // this will trigger when enter is pressed
+
+        // loop here of every card dealt and checking if winner - will keep looping through the deckOfCards
+
+        while (true) {
+            Card newCard = dealCard(); // getting the next card
+            System.out.println("Card dealt: " + newCard); // change wording on this WIP
+
+            // checking if there was a previous card in dealtCards arrayList and if they match
+            if (!dealtCards.isEmpty() && dealtCards.getLast().getSymbol().equals(newCard.getSymbol())) {
+                System.out.println("Snap! You win!");
+                break;
+            }
+
+            dealtCards.add(newCard); // storing the card for comparison
+//            System.out.println("Press Enter to deal the next card...");
+            scanner.nextLine(); // getting user enter input
+        }
+
+
+    } // end playGame
+
+
+
 
     public static void main(String[] args) {
 
-        // welcome to Java Snap
-        // How to play Java Snap:
-        // To start the game press enter and a card will be dealt to you.
-        // hit enter again to be dealt another card.
-        // if you get two cards in a row are of the same symbol you will win the game
-        // when you are ready hit enter to be dealt your first card
+        // when this file is run the game should start, a fresh new CardGame - deckOfCards should get created.
+        Snap snapGame = new Snap();
+        snapGame.playGame();
 
-
-        // two cards in a row get dealt with the same symbol, for example 10 & 10, J & J etc....
-        // Well done, you won the game, would you like to start a new game? Y/N?
 
 
     } // snap main
